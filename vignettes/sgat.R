9bffdde890abe8e3c048e156fa4617c13449ea99
matiaspoullain <58349277+matiaspoullain@users.noreply.github.com>
2021-02-19 14:47:47 -0300

    new vignette

--- vignettes/sgat.R
+++ vignettes/sgat.R
@@ -14,3 +14,20 @@ initialization_sgat()
 restaurants <- ushuaia_restaurants()
 head(restaurants)
 
+## ----bar 1.1, message=FALSE, warning=FALSE------------------------------------
+
+bar_1 <- sgat("coutume, 47 rue de babylone, 75007 paris, france", tiempo.espera = 20)
+head(bar_1)
+
+## ----bar 2.2, echo = TRUE, message=FALSE, warning=FALSE-----------------------
+bar_2 <- sgat("Moustacchio, San Martín 298, Ushuaia", tiempo.espera = 20)
+bar_2
+
+## ----bar 3.2, message=FALSE, warning=FALSE------------------------------------
+bar_3 <- sgat_day("coutume, 47 rue de babylone, 75007 paris, france", "martes")
+head(bar_3)
+
+## ----tripadvisor, message=FALSE, warning=FALSE--------------------------------
+pinamar <- tripadvisor_places("Pinamar", 10)
+pinamar
+
